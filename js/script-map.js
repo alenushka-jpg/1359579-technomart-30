6833feff2c3fdd20d4a48087df854b406150f9eb
alenushka-jpg <74616466+alenushka-jpg@users.noreply.github.com>
2021-03-22 20:30:38 +0300

    Правки

--- js/script-map.js
+++ js/script-map.js
@@ -1,11 +1,8 @@
-
-// выпадающая модал-карта
 const mapButton = document.querySelector(".company-contacts-img");
 const mapPopup = document.querySelector(".modal-map");
 const mapClose = mapPopup.querySelector(".modal-close");
 
 
-// выпадающая модал-карта
 mapButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   mapPopup.classList.add("modal-show");
